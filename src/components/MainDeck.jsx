7ee9da82588474de063c3316f620932e46dfe5fa
Henry Medeiros <henrymedeiros77@gmail.com>
2025-02-18 23:34:11 -0300

    MainDeck area changes

--- src/components/MainDeck.jsx
+++ src/components/MainDeck.jsx
@@ -1,41 +1,34 @@
 import React from "react";
-import Card from "./Card.jsx";
 
-export default function MainDeck({ deck, setDeck, setCardHovered}) {
-  function removeCard(cardIndex) {
-    var array = [...deck]; // make a separate copy of the array
-    if (cardIndex !== -1) {
-      array.splice(cardIndex, 1);
-      console.log(array)
-      setDeck(array);
+import CardsGrid from "./CardsGrid.jsx";
+import AreaTitle from "./AreaTitle.jsx";
+
+export default function MainDeck({ deck, setDeck, setCardHovered }) {
+    function removeCard(cardIndex) {
+        var array = [...deck]; // make a separate copy of the array
+        if (cardIndex !== -1) {
+            array.splice(cardIndex, 1);
+            console.log(array);
+            setDeck(array);
+        }
     }
-  }
 
-  return (
-    <div className="MainDeck area">
-      <div className="areaTitle">Main Deck: {deck.length}</div>
-      <div className="Deck">
-        
-        {deck ? (
-        deck.map((card, index) => {
-          return (
-            <div
-              className="card"
-              onClick={() => {
-                removeCard(index)
-              }}
-              onMouseOver={() => {setCardHovered(card)}}
-            >
-              <Card cardData={card} index={index} />
-            </div>
-          );
-        })
-      ) : (
-        console.log('oxe')
-      )}
-        
-      
-      </div>
-    </div>
-  );
+    return (
+        <div className="MainDeck">
+            <AreaTitle
+                id="mainDeckAreaTitle"
+                title="Main Deck"
+                areaCardCount={deck.length}
+            ></AreaTitle>
+            <CardsGrid
+                cards={deck}
+                setCards={setDeck}
+                setCardHovered={setCardHovered}
+                minGridRows={4}
+                maxGridRows={5}
+                minGridColumns={10}
+                maxGridColumns={12}
+            ></CardsGrid>
+        </div>
+    );
 }
