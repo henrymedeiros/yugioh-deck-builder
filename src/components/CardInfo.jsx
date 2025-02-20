c817b2cc5fe3d783461f0d63c293a56dc45449c0
Henry Medeiros <henrymedeiros77@gmail.com>
2025-02-19 22:50:07 -0300

    style: improving layout of card info area

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -13,16 +13,25 @@ export default function CardInfo() {
     });
   }
 
+  function parseTypeline(typeline) {
+    return typeline.map((typelineItem, index) => {
+      if (index === typeline.length - 1) {
+        return typelineItem;
+      }
+      return typelineItem + "/";
+    });
+  }
+
 
   return (
-    <div className="CardData border border-black p-2">
+    <div className="CardData border border-black p-2 ">
       {selectedCard &&
-        <div className="text-center w-[90%] mx-[5%] my-[10px]">
+        <div className={`text-center w-[90%] mx-[5%] my-[10px] py-1.5 rounded bg-black`}>
           {selectedCard.name}
         </div>
       }
       {selectedCard ? (
-        <div className='max-w-[420px] w-[70%] min-w-[180px] mx-auto'>
+        <div className='max-w-[420px] w-[65%] min-w-[180px] mx-auto'>
           <img
             src={selectedCard.imgUrlBig}
             alt={selectedCard.name}
@@ -33,8 +42,8 @@ export default function CardInfo() {
         <span></span>
       )}
       {selectedCard &&
-        <div className="text-center w-[90%] mx-[5%] my-[10px]">
-          [{selectedCard.race}/{selectedCard.type}]
+        <div className="flex justify-center items-center gap-2 text-center w-[90%] mx-[5%] my-[10px]">
+          {selectedCard.typeline ? <span>[{parseTypeline(selectedCard.typeline)}]</span> : <span>[{selectedCard.race}/{selectedCard.type}]</span>}
         </div>
       }
       {selectedCard &&
@@ -46,7 +55,10 @@ export default function CardInfo() {
         </div>
       }
       {selectedCard &&
-        <div className='bg-black bg-opacity-5 text-white p-4'>{parseCardDesc(selectedCard.desc)}</div>
+        <div className='bg-black w-[90%] mx-auto bg-opacity-5 text-white p-4'>
+          <div className='text-center text-xs mb-4'>ID: {selectedCard.id }</div>
+          <div className='text-sm'>{parseCardDesc(selectedCard.desc)}</div>
+        </div>
       }
     </div>
   );
